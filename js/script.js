b23aa4ee14246fc72af2d1f623c41c38866cbad1
DaOnlyDelta <delta.leskovar@gmail.com>
2025-12-18 22:45:20 +0100

    feat: Update layout and enhance visual effects on homepage

--- js/script.js
+++ js/script.js
@@ -2,12 +2,16 @@ const container = document.getElementById('imgContainer');
 const slide1 = document.getElementById('slide1');
 const slide2 = document.getElementById('slide2');
 const slide3 = document.getElementById('slide3');
-const slides = [slide1, slide2, slide3];
 const image = document.getElementById('centerImage');
 const startPoint = 50; // scrollY when it snaps to center
-const endPoint = 2200; // scrollY when it snaps out of center
+const endPoint = 2300; // scrollY when it snaps out of center
 const switchPoints = [200, 900, 1600]; // scrollY thresholds to change image
-let currentSlide = 0;
+let currentSlide = 1;
+
+const btContainer = document.getElementById('bt');
+const bt1 = document.getElementById('bt1');
+const bt2 = document.getElementById('bt2');
+const bt3 = document.getElementById('bt3');
 
 window.addEventListener('scroll', () => {
     const scroll = window.scrollY;
@@ -16,40 +20,53 @@ window.addEventListener('scroll', () => {
     if (scroll >= startPoint && scroll < endPoint) {
         container.classList.add('fixed');
         container.style.top = '';
-        container.style.transform = '';
+        btContainer.classList.add('fixed');
+        btContainer.style.top = '';
     } else if (scroll < startPoint) {
         container.classList.remove('fixed');
         container.style.top = '';
-        container.style.transform = '';
+        btContainer.classList.remove('fixed');
+        btContainer.style.top = '';
     } else if (scroll >= endPoint) {
         container.classList.remove('fixed');
-        container.style.top = `${endPoint + 10}px`;
-        container.style.transform = 'translateX(-50%)';
+        container.style.top = `${endPoint - 115}px`;
+        btContainer.classList.remove('fixed');
+        btContainer.style.top = `${endPoint + 540}px`;
     }
 
     let newSlide = currentSlide; // Track the new slide index
 
     // Switch between images based on scroll
     if (scroll < switchPoints[1]) {
-        newSlide = 0;
-        slides[0].style.opacity = '1';
-        slides[0].style.top = '0';
-        slides[1].style.top = '-10%';
-        slides[1].style.opacity = '0';
-    } else if (scroll < switchPoints[2]) {
         newSlide = 1;
-        slides[0].style.top = '10%';
-        slides[0].style.opacity = '0';
-        slides[1].style.top = '0';
-        slides[1].style.opacity = '1';
-        slides[2].style.opacity = '0';
-        slides[2].style.top = '-10%';
-    } else {
+        slide1.style.opacity = '1';
+        slide1.style.top = '0';
+        slide2.style.top = '-5%';
+        slide2.style.opacity = '0';
+
+        bt1.style.opacity = '1';
+        bt2.style.opacity = '0';
+    } else if (scroll < switchPoints[2]) {
         newSlide = 2;
-        slides[1].style.top = '10%';
-        slides[1].style.opacity = '0';
-        slides[2].style.top = '0';
-        slides[2].style.opacity = '1';
+        slide1.style.top = '5%';
+        slide1.style.opacity = '0';
+        slide2.style.top = '0';
+        slide2.style.opacity = '1';
+        slide3.style.opacity = '0';
+        slide3.style.top = '-5%';
+
+        bt1.style.opacity = '0';
+        bt2.style.opacity = '1';
+        bt3.style.opacity = '0';
+    } else {
+        newSlide = 3;
+        slide2.style.top = '5%';
+        slide2.style.opacity = '0';
+        slide3.style.top = '0';
+        slide3.style.opacity = '1';
+
+        bt2.style.opacity = '0';
+        bt3.style.opacity = '1';
     }
 
     // Only call fadeGlow if the slide has changed
@@ -63,5 +80,5 @@ function fadeGlow() {
     container.classList.add('fade');
     setTimeout(() => {
         container.classList.remove('fade');
-    }, 200);
+    }, 250);
 }
